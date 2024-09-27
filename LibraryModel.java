697dc452e81dabdd08c7f0cb30c6ff4304e25672
Fay Garcia <faywagstaff@hotmail.com>
2024-09-27 14:52:18 -0700

    renamed file

---
+++ LibraryModel.java
@@ -0,0 +1,134 @@
+import java.util.Random;
+import java.util.*;
+
+
+
+/*
+ * Author(s):
+ * File:
+ * Course:
+ * Description:
+ */
+
+
+public class Collection {
+
+	ArrayList<Book> books;
+
+	// instance variables
+	
+	
+	
+	
+	
+	/*
+	 * constructor
+	 */
+	public Collection() {
+		
+	}
+	
+	
+	
+	
+	
+	/*
+	 * find all books with a given search criteria
+	 * 
+	 * *going to need parameters to find books that have common factors*
+	 */
+	public void search() {
+		
+	}
+	
+	
+	
+	
+	
+	/*
+	 * create a book to add to the collection
+	 * 
+	 * *going to need parameters to create book*
+	 */
+	public void addBook() {
+		
+		// Book(title, author, rating)
+		// add to collection
+	}
+	
+	
+	
+	
+	
+	/*
+	 * edit the read status of the book
+	 * 
+	 * *going to need parameters to find book*
+	 */
+	public void setToRead() {
+		
+		// 
+		
+	}
+	
+	
+	
+	
+	
+	/*
+	 * edit the integer 1-5 rating of the book
+	 * 
+	 * * going to need parameters to find book*
+	 */
+	public void rate() {
+		
+		// book.updateRating()
+	}
+	
+	
+	
+	
+	
+	/*
+	 * return entire collection of books given a specific return criteria
+	 * 
+	 * *probably going to need helper methods for different criteria*
+	 * 
+	 * *going to need to figure out return type*
+	 * 
+	 * *going to need parameter to decipher return format*
+	 */
+	public void getBooks() {
+		
+	}
+	
+	
+	
+	
+	
+	/*
+	 * find a random book to return to the user
+	 * (maybe use shuffle to mix collection and return index 0?)
+	 * (maybe use random.nextInt to pick a random index?)
+	 * 
+	 * *going to need to return either data set containing info or Book object itself*
+	 */
+	public void suggestRead() {
+		
+	}
+	
+	
+	
+	
+	
+	/*
+	 * read a file and add books from file info
+	 * 
+	 *  *going to need a parameter for input file name*
+	 */
+	public void addBooks() {
+		
+	}
+	
+	
+}
