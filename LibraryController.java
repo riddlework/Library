6b5dbfefa71f99130c11bacaffd8c669ccf3965a
Fay Garcia <faywagstaff@hotmail.com>
2024-09-27 15:51:39 -0700

    small changes

--- LibraryController.java
+++ LibraryController.java
@@ -8,14 +8,12 @@
 
 public class LibraryController {
 
-	private LibraryModel; 
+	private LibraryModel model;
 
 
 
 	
-	public LibraryController(LibraryModel model) 
-		this.model = model;
-	}
+	public LibraryController(LibraryModel model) { this.model = model; }
 
 
 
