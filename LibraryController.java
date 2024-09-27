fdf063a3c715f0f01ba0da883d058692fe7276c2
benyurek <149546012+benyurek@users.noreply.github.com>
2024-09-27 15:40:45 -0700

    Update LibraryController.java

--- LibraryController.java
+++ LibraryController.java
@@ -8,16 +8,49 @@
 
 public class LibraryController {
 
+	private LibraryModel; 
+
+
+
 	
-	/*
-	 * constructor
-	 */
-	public LibraryController() {
-		
+	public LibraryController(LibraryModel model) 
+		this.model = model;
 	}
-	
-	
-	
+
+
+
+
+	public void search() {
+		model.search();		
+	}
+
+	public void addBook() {
+		model.addBook();
+	}
+
+	public void setToRead() {
+		model.setToRead();	
+	}
+
+	public void rate() {
+		model.rate();
+	}
+
+	public void getBooks() {
+		model.getBooks();
+	}
+
+	public void suggestRead() {
+		model.suggestRead();
+	}
+
+	public void addBooks() {
+		model.addBooks();
+	}
+
+
+
+
 	
 	
 	
