a27dccb486191d0500da173151d92a613488f7a1
riddlework <82472185+riddlework@users.noreply.github.com>
2024-11-11 13:49:49 -0700

    made exception a different class

--- LibraryModel.java
+++ LibraryModel.java
@@ -232,13 +232,4 @@ public class LibraryModel {
 }
 
 
-/**
- * An exception to be thrown when no book of a given criterion (or no criteria) exists
- */
-class NoSuchBookException extends Exception {
-	// constructor that accepts a message
-	public NoSuchBookException (String message) {
-		super(message);
-	}
-}
 
