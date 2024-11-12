ba8695515af8ead5913b3a578ee8facd979f5c9c
riddlework <82472185+riddlework@users.noreply.github.com>
2024-11-12 13:12:38 -0700

    init gui file

---
+++ MyLibraryGUI.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyLibraryGUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Outer frame");
+        frame.setLayout(new FlowLayout());
+    }
+}
